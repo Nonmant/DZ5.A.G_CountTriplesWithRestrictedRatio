2acd6925089ae61e5049de097a7067cc2e414e24
Anton Bolotskih <89161424838@yandex.ru>
2021-12-04 00:42:07 +0300

    No idea, why it worked Passed

--- funcs.cpp
+++ funcs.cpp
@@ -5,30 +5,30 @@
 
 void parseFile(std::istream & input, std::ostream & output){
 
-    int n, k;
+    long long n, k;
     input >> n >> k;
 
     //! @brief number, times
-    std::map<int, int> numbers;
-    int number;
+    std::map<long long , long long> numbers;
+    long long number;
     for(int i = 0; i<n; ++i){
         input >> number;
         ++numbers[number];
     }
     auto numsSize = numbers.size();
-    std::vector<std::pair<int,int>> numVect(numsSize);
+    std::vector<std::pair<long long,long long>> numVect(numsSize);
     std::copy_n(numbers.begin(), numsSize, numVect.begin());
 
-    unsigned long long count = 0, duplicates = 0;
+    long long count = 0, duplicates = 0;
     auto right = numVect.begin();
     for(auto left = numVect.begin(); left!=numVect.end(); ++left){
-        unsigned long long rightMax = left->first*k;
+        long long rightMax = left->first*k;
         while (right!=numVect.end() && rightMax>=right->first){
             if(right->second>1)
                 ++duplicates;
             ++right;
         }
-        auto rangeLen = std::distance(left,right);
+        long long rangeLen = std::distance(left,right);
         if(left->second>=3)// 1,1,1
             ++count;
         if(left->second>=2){// 1,1,* | 1,*,1 | *,1,1
@@ -39,7 +39,7 @@ void parseFile(std::istream & input, std::ostream & output){
         count+=duplicates*3;//each duplicate > left generates 3 variants
 
         //Variants with unique numbers
-        count+=(rangeLen-1)*(rangeLen-2)*3;//>=0
+        count+=std::max((rangeLen-1)*(rangeLen-2)*3,(long long)(0));
     }
 
     output << count;
